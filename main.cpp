63eec66d93b595dac02041e12bb40f2031dee066
joeyb <bongarboy2@gmail.com>
2020-05-06 21:09:02 -0700

    im illiterate

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ int main() {
     auto elapsed = chrono::high_resolution_clock::now() - startTime; //endtime
     long long timeTotal = chrono::duration_cast<chrono::microseconds>(elapsed).count();
     double time = timeTotal; // 1000000.00;
-    cout<< "Algorithim took: "<< time<< " Microseconds. Results displayed above^" <<endl<<endl;
+    cout<< "Algorithm took: "<< time<< " Microseconds. Results displayed above^" <<endl<<endl;
     trigram obj2;
     obj2.CreateIndex(yew);
     auto startTime2 = chrono::high_resolution_clock::now();
@@ -24,7 +24,7 @@ int main() {
     auto elapsed2 = chrono::high_resolution_clock::now() - startTime2; //endtime
     long long timeTotal2 = chrono::duration_cast<chrono::microseconds>(elapsed2).count();
     double time2 = timeTotal2 ;// 1000000.00;
-    cout<< "ALgorithim took: " << time2 << " Microseconds. Results displayed above^" <<endl;
+    cout<< "Algorithm took: " << time2 << " Microseconds. Results displayed above^" <<endl;
 
     return 0;
 }
